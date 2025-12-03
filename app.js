fb354731001f53045b0259e75105d8fcb5f140dc
UrielX16-git <leiru_uriel16@hotmail.com>
2025-12-02 19:44:18 -0600

    Mejora en la forma de buscar

--- app.js
+++ app.js
@@ -56,44 +56,43 @@ $(document).ready(function () {
                 success: function (response) {
                     let papers = JSON.parse(response);
                     let template = '';
-                    let template_bar = '';
 
-                    papers.forEach(paper => {
-                        let imagenPath = paper.Imagen.replace('../', '');
-                        template += `
-                            <div class="paper-card" paperId="${paper.ID}">
-                                <div class="row">
-                                    <div class="col-md-3 paper-img-container">
-                                        <img src="${imagenPath}" alt="${paper.Nombre}" class="paper-img">
-                                    </div>
-                                    <div class="col-md-9 paper-content">
-                                        <div class="paper-header">
-                                            <h4 class="paper-title">${paper.Nombre}</h4>
-                                            <span class="paper-date">${paper.Fecha}</span>
+
+                    if (papers.length === 0) {
+                        template = '<div class="alert alert-warning" role="alert" style="text-align: center; margin-top: 20px;">No se ha encontrado el paper...</div>';
+                    } else {
+                        papers.forEach(paper => {
+                            let imagenPath = paper.Imagen.replace('../', '');
+                            template += `
+                                <div class="paper-card" paperId="${paper.ID}">
+                                    <div class="row">
+                                        <div class="col-md-3 paper-img-container">
+                                            <img src="${imagenPath}" alt="${paper.Nombre}" class="paper-img">
                                         </div>
-                                        <div class="paper-authors">
-                                            Autores: ${paper.Autores}
-                                        </div>
-                                        <div class="paper-description">
-                                            ${paper.Explicacion}
+                                        <div class="col-md-9 paper-content">
+                                            <div class="paper-header">
+                                                <h4 class="paper-title">${paper.Nombre}</h4>
+                                                <span class="paper-date">${paper.Fecha}</span>
+                                            </div>
+                                            <div class="paper-authors">
+                                                Autores: ${paper.Autores}
+                                            </div>
+                                            <div class="paper-description">
+                                                ${paper.Explicacion}
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                        `;
+                            `;
+                        });
+                    }
 
-                        template_bar += `
-                            <li>${paper.Nombre}</li>
-                        `;
-                    });
 
-                    $('#paper-result').removeClass('d-none');
-                    $('#container').html(template_bar);
-                    $('#papers-container').html(template);
+                    $('#papers').html(template);
                 }
             });
         } else {
-            $('#paper-result').addClass('d-none');
+
             listarPapers(); // Volver a listar todos si no hay búsqueda
         }
     });
